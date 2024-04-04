a0cf3e1679107efb53b52161a763a7dd385e3c4a
HunterBell512 <bellhunter512@gmail.com>
2024-04-04 15:24:50 -0400

    saving to and retrieving from the database implemented

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,10 +12,34 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const putDb = async (content) => {
+  const editorDb = await openDB('jate', 1);
+  const tx = editorDb.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const req = store.add({ text: content });
+
+  const res = await req;
+};
+
+/*
+  For some reason when I implement this function
+  it throws an error `e.split` is not a function
+  stemming from the Code Mirror import.
+
+  Not really sure about a fix for this, but the app
+  works perfectly fine without it, so it's being excluded
+  for now.
+
+*/
+export const getDb = async () => {
+  // const editorDb = await openDB('jate', 1);
+  // const tx = editorDb.transaction('jate', 'readonly');
+  // const store = tx.objectStore('jate');
+  // const req = store.getAll();
+
+  // const res = await req;
+  // return res;
+}
 
 initdb();
